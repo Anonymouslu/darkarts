24f2a7edcebdb067fbdd90c4a18a57572aa9fb05
evolu <43544769@qq.com>
2023-04-27 11:07:38 +0800

    首次提交，创建evolu的黑魔法

---
+++ src/main/java/com/evolu/darkarts/darkarts/DarkartsApplication.java
@@ -0,0 +1,13 @@
+package com.evolu.darkarts.darkarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evolu/darkarts/darkarts/DarkartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolu.darkarts.darkarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
